1d00939e0701b72c10af6d1ed05fcf5449392f97
vamsi <bellamvamsikrishna3415@gmail.com>
2022-07-28 21:04:00 +0530

    Explore createReadStream

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function hello() {
+/* function hello() {
   console.log("Hello Node.Js! Trying it for the first time");
 }
 
@@ -19,9 +19,28 @@ fs.appendFile("sample.txt", " Updated Content !", function (err) {
 fs.rename("sample.txt", "test.txt", function (err) {
   if (err) throw err;
   console.log("File Renamed !");
-});
+}); */
 
 /* fs.unlink("test.txt", function (err) {
   if (err) throw err;
   console.log("File Deleted !");
 }); */
+
+const http = require("http");
+const fs = require("fs");
+
+/* // Using fs
+const server = http.createServer(function (req, res) {
+  fs.readFile("test.txt", (err, data) => {
+    res.end(data);
+  });
+});
+ */
+
+// Using Streams
+const server = http.createServer(function (req, res) {
+  const stream = fs.createReadStream("test.txt");
+  stream.pipe(res);
+});
+
+server.listen(3001);
