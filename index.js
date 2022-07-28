38d1b9850794bd72f507dfcf9faacb61a289a0c4
vamsi <bellamvamsikrishna3415@gmail.com>
2022-07-28 20:50:23 +0530

    Explore fs module

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ function hello() {
 }
 
 hello();
+
+const fs = require("fs");
+
+fs.writeFile("sample.txt", "Hello, Welcome to Node !", function (err) {
+  if (err) throw err;
+  console.log("File created !");
+});
+
+fs.appendFile("sample.txt", " Updated Content !", function (err) {
+  if (err) throw err;
+  console.log("File Updated !");
+});
+
+fs.rename("sample.txt", "test.txt", function (err) {
+  if (err) throw err;
+  console.log("File Renamed !");
+});
+
+/* fs.unlink("test.txt", function (err) {
+  if (err) throw err;
+  console.log("File Deleted !");
+}); */
